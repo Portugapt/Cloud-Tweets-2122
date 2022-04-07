ed7d55e91dfd18baab5ffbdda8d6d6c44c827a62
renato <renato.joao9@gmail.com>
2022-04-07 14:19:35 +0200

    work on services

---
+++ services/list-tweet-username/README.md
@@ -0,0 +1,2 @@
+## Set credentials to access Big Query:
+export GOOGLE_APPLICATION_CREDENTIALS="/home/bosutike/cloud/Cloud-Tweets-2122/services/pythonBigQuery_credentials.json"
---
+++ services/list-tweet-username/server.py
@@ -0,0 +1,46 @@
+import os
+
+# from google.cloud import bigquery
+
+from flask import Flask, render_template
+import grpc
+
+from proto_pb2 import ClearListRequest, Tweet
+from proto_pb2_grpc import ClearTweetsStub
+
+app = Flask(__name__)
+
+# def query_stackoverflow():
+#     client = bigquery.Client()
+#     query_job = client.query(
+#         """
+#         SELECT *
+#         FROM `cadeira-nuvem-2122.bq_cloud_2122.db_users`
+#         LIMIT 10"""
+#     )
+
+#     results = query_job.result()  # Waits for job to complete.
+
+#     for row in results:
+#         print(row)
+CLEAR_TWEET_LIST_HOST = os.getenv("CLEAR_TWEET_LIST_HOST", "localhost")
+CLEAR_TWEET_LIST_PORT = os.getenv("CLEAR_TWEET_LIST_PORT", "50060")
+clear_tweet_list_channel = grpc.insecure_channel(f"{CLEAR_TWEET_LIST_HOST}:{CLEAR_TWEET_LIST_PORT}")
+clear_tweet_list_client = ClearTweetsStub(clear_tweet_list_channel)
+
+
+@app.route("/")
+def render_homepage():
+    clear_list_request = ClearListRequest(
+        tweet_list=[Tweet(id=0, text="Some text!", user="username")]
+    )
+    clear_list_response = clear_tweet_list_client.ClearTweet(
+        clear_list_request
+    )
+
+    print(clear_list_response)
+
+    return "<h1>HelloWorld</h1>"
+
+#if __name__ == "__main__":
+#    query_stackoverflow()
