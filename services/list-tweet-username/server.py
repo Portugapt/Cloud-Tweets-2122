397a6cdc1989c818dad0062a72dac76f210332b9
renato <renato.joao9@gmail.com>
2022-04-18 13:07:07 +0200

    add limit to list-tweet-username

--- services/list-tweet-username/server.py
+++ services/list-tweet-username/server.py
@@ -13,8 +13,8 @@ from clear_tweet_proto_pb2_grpc import ClearTweetsStub
 app = Flask(__name__)
 
 
-def query_stackoverflow(username):
-    key_path = os.getenv("GOOGLE_ACCOUNT_KEY")
+def query_stackoverflow(username, limit):
+    key_path = os.getenv("GOOGLE_ACCOUNT_KEY", "../../../pythonBigQuery_credentials.json")
 
     credentials = service_account.Credentials.from_service_account_file(
         key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
@@ -26,11 +26,12 @@ def query_stackoverflow(username):
             SELECT tweetId, username, tweettext
             FROM `cadeira-nuvem-2122.bq_cloud_2122.db_global`
             WHERE username = @username
-            LIMIT 2"""
+            LIMIT @limit"""
     
     job_config = bigquery.QueryJobConfig(
         query_parameters=[
             bigquery.ScalarQueryParameter("username", "STRING", username),
+            bigquery.ScalarQueryParameter("limit", "INT64", limit),
         ]
     )
 
@@ -50,10 +51,10 @@ CLEAR_TWEET_LIST_PORT = os.getenv("CLEAR_TWEET_LIST_PORT", "50060")
 clear_tweet_list_channel = grpc.insecure_channel(f"{CLEAR_TWEET_LIST_HOST}:{CLEAR_TWEET_LIST_PORT}")
 clear_tweet_list_client = ClearTweetsStub(clear_tweet_list_channel)
 
-
 @app.route("/list-tweet-username/<username>")
-def render_homepage(username):
-    results = query_stackoverflow(username)
+@app.route("/list-tweet-username/<username>/<limit>")
+def render_homepage(username, limit='1000'):
+    results = query_stackoverflow(username, limit)
 
     clear_list_request = ClearListRequest(
         tweet_list=results
