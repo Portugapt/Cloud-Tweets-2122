cc3ef092edcd8addf756e9d078969f96e61233ed
Portugapt <monteiro.joao.ps@gmail.com>
2022-06-13 11:48:06 +0100

    fix location

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import pulumi
 from pulumi_gcp import storage
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('tweets-landing-zone')
+bucket = storage.Bucket('tweets-landing-zone', location = 'EUROPE-WEST1')
 
 # Export the DNS name of the bucket
 pulumi.export('tweets-landing-zone',  bucket.url)
