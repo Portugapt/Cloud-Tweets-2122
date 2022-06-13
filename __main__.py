01e6fcc18d5cf531b8b97cc7a312f6b395d74b3e
Portugapt <monteiro.joao.ps@gmail.com>
2022-06-13 16:01:42 +0100

    dataset creation

--- __main__.py
+++ __main__.py
@@ -1,9 +1,21 @@
 import pulumi
-from pulumi_gcp import storage
+from pulumi_gcp import storage, bigquery
+
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('tweets-landing-zone', 
-                        location='EU')
+bucket_lz = storage.Bucket(resource_name='tweets-landing-zone',
+                           location='EU',
+                           name='tweets-landing-zone')
+
+bucket_dataproc = storage.Bucket(resource_name='tweets-dataproc',
+                                 location='EU',
+                                 name='tweets-dataproc')
 
 # Export the DNS name of the bucket
-pulumi.export('tweets-landing-zone',  bucket.url)
+pulumi.export('tweets-landing-zone',  bucket_lz.url)
+pulumi.export('tweets-dataproc',  bucket_dataproc.url)
+
+dataset_tweets = bigquery.Dataset(resource_name='bq_cloud_2122',
+                                  location='europe-west1',
+                                  dataset_id='bq_cloud_2122',
+                                  labels={'type':'bq dataset', 'env':'dev'})
