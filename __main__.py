cacd5b81b17e96efd4599340eb6b23220e7bbf29
Portugapt <monteiro.joao.ps@gmail.com>
2022-06-13 18:26:52 +0100

    test

--- __main__.py
+++ __main__.py
@@ -25,6 +25,7 @@ pulumi.export('tweets-dataproc',  bucket_dataproc.url)
 def _load_json_schema(path: str) -> str:
     with open(path, "r") as file:
         fileData  = file.read()
+        print(json.loads(fileData))
         return json.loads(fileData)
 
 dataset_tweets = bigquery.Dataset(resource_name='bq_cloud_2122',
@@ -33,24 +34,21 @@ dataset_tweets = bigquery.Dataset(resource_name='bq_cloud_2122',
                                   labels={'type': 'bq-dataset', 'env': 'default'})
 
 db_admin_user = bigquery.Table(resource_name='db_admin_user',
-                               opts=pulumi.ResourceOptions(protect=True,
-                                                           depends_on=dataset_tweets),
+                               opts=pulumi.ResourceOptions(protect=True),
                                dataset_id=dataset_tweets.dataset_id,
                                table_id='db_admin_user',
                                friendly_name='db_admin_user',
                                schema=_load_json_schema('resources/db_admin_user_schema.json'))
 
 db_global = bigquery.Table(resource_name='db_global',
-                               opts=pulumi.ResourceOptions(protect=True,
-                                                           depends_on=dataset_tweets),
+                               opts=pulumi.ResourceOptions(protect=True),
                                dataset_id=dataset_tweets.dataset_id,
                                table_id='db_global',
                                friendly_name='db_global',
                                schema=_load_json_schema('resources/db_global_schema.json'))
 
 db_readfiles = bigquery.Table(resource_name='db_readfiles',
-                               opts=pulumi.ResourceOptions(protect=True,
-                                                           depends_on=dataset_tweets),
+                               opts=pulumi.ResourceOptions(protect=True),
                                dataset_id=dataset_tweets.dataset_id,
                                table_id='db_readfiles',
                                friendly_name='db_readfiles',
