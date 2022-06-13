5447e43f59bdaa0d38fcec086dc408597e511d50
Portugapt <monteiro.joao.ps@gmail.com>
2022-06-13 11:53:27 +0100

    yaaar

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import pulumi
 from pulumi_gcp import storage
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('tweets-landing-zone', location = 'EUROPE-WEST1')
+bucket = storage.Bucket('tweets-landing-zone', location='EU')
 
 # Export the DNS name of the bucket
 pulumi.export('tweets-landing-zone',  bucket.url)
