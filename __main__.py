d0466ec3c582ec3cb5a22f6274c4067b649cf2a4
Portugapt <monteiro.joao.ps@gmail.com>
2022-06-13 12:00:20 +0100

    gcp:storage:Bucket (tweets-landing-zone):     error: could not validate provider configuration: 1 error occurred:     * Invalid or unknown key

--- __main__.py
+++ __main__.py
@@ -2,7 +2,8 @@ import pulumi
 from pulumi_gcp import storage
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('tweets-landing-zone', location='EU')
+bucket = storage.Bucket('tweets-landing-zone', 
+                        location='EU')
 
 # Export the DNS name of the bucket
 pulumi.export('tweets-landing-zone',  bucket.url)
